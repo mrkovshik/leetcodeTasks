0f3cbaff23fd352c3ee9156b6d9f2218601157a3
kolya <kovshik@inbox.ru>
2022-10-11 16:50:58 +0300

    Gogog

---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// определите переменные ver, id, pi ver = v0.0.1 id = 0 pi = 3.1415
+	// ...
+	ver := string("v0.0.1")
+	id := int(0)
+	pi := float32(2.1415)
+	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
+}
